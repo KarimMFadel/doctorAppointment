a18a23c650c845c5fcdf03c20616076324766ffe
karimmfadel <karimmm.fadel@gmail.com>
2019-12-17 06:32:26 +0200

    Get All methods

--- common-service/src/main/java/com/extremesolution/commonservice/service/base/BaseService.java
+++ common-service/src/main/java/com/extremesolution/commonservice/service/base/BaseService.java
@@ -1,9 +1,12 @@
 package com.extremesolution.commonservice.service.base;
 
+import java.util.List;
+
 public interface BaseService<T> {
 	
 	public String save(T object);
 	public void update(String id, T object);
 	public T get(String id);
 	public void delete(String id);
+	public List<T> getAllDocuments();
 }
